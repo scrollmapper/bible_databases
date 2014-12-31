e176f9cbad07c675a055f6d637daecd351adc1cf
Andrew Wippler <andrew.wippler@gmail.com>
2014-12-30 16:00:41 -0800

    bug fixes

--- web/bible_to_sql.php
+++ web/bible_to_sql.php
@@ -1,5 +1,31 @@
 <?php
 
+function convertToNumber($book = NULL, $database = NULL) {
+		
+	if (!$database) { die('you forgot to specify the database in your bible_to_sql call.'); }
+		
+	$query = "SELECT B from bible.key_abbreviations_english WHERE A=?";
+	$query2 = "SELECT b from bible.key_english WHERE n=?";
+	
+	$stmt = $database->stmt_init();
+	$stmt->prepare($query);
+	$stmt->bind_param("s", $book);
+	$stmt->execute();
+        $result = $stmt->get_result();
+        if ($row = $result->fetch_array(MYSQLI_NUM)) {
+            return $row[0];
+        } else {
+			$stmt->prepare($query2);
+			$stmt->bind_param("s", $book);
+			$stmt->execute();
+			$result = $stmt->get_result();
+			if ($row = $result->fetch_array(MYSQLI_NUM)) {
+				return $row[0];
+			}
+		}
+     $stmt->close();
+}
+
 //JOSHUA 1:8-10 to 0601008-0601010
 
 //return book number
@@ -11,11 +37,15 @@ class bible_to_sql {
 	protected $endverse = 999;
 	protected $range = FALSE;
 	
-	public function __construct( /*String*/ $string = null, $range = FALSE) {
+	public function __construct($string = NULL, $range = FALSE, $database = NULL) {
+		
+		//places a . between book name and reference. 
+		//i.e. changes "Song of Solomon 9:6" to "Song of Solomon.9:6"
+		$string = preg_replace('/\s(\S*)$/', '.$1', trim($string)); //trim end for sanitization.
 				
 		//split
-		$separatedArray = explode(" ",$string);
-		$this->book = $this->bookToNumber($separatedArray[0]);
+		$separatedArray = explode(".",$string);
+		$this->book = $this->addZeros(convertToNumber($separatedArray[0], $database),2);
 		
 		//split chapter and verse
 		$separatedVerse = explode(":",$separatedArray[1]);
@@ -59,217 +89,17 @@ class bible_to_sql {
     
     public function bookToNumber($book) {
 		
-		switch($book) {
-			case "Genesis":
-				return "01";
-				break;
-			case "Exodus":
-				return "02";
-				break;
-			case "Leviticus":
-				return "03";
-				break;
-			case "Numbers":
-				return "04";
-				break;
-			case "Deuteronomy":
-				return "05";
-				break;
-			case "Joshua":
-				return "06";
-				break;
-			case "Judges":
-				return "07";
-				break;
-			case "Ruth":
-				return "08";
-				break;
-			case "1 Samuel":
-				return "09";
-				break;
-			case "2 Samuel":
-				return "10";
-				break;
-			case "1 Kings":
-				return "11";
-				break;
-			case "2 Kings":
-				return "12";
-				break;
-			case "1 Chronicles":
-				return "13";
-				break;
-			case "2 Chronicles":
-				return "14";
-				break;
-			
-			case "Ezra":
-				return "15";
-				break;
-			case "Nehemiah":
-				return "16";
-				break;
-			case "Esther":
-				return "17";
-				break;
-			case "Job":
-				return "18";
-				break;
-			case "Psalms":
-				return "19";
-				break;
-			case "Proverbs":
-				return "20";
-				break;
-			case "Ecclesiastes":
-				return "21";
-				break;
-			case "Song of Solomon":
-				return "22";
-				break;
-			case "Isaiah":
-				return "23";
-				break;
-			case "Jeremiah":
-				return "24";
-				break;
-			case "Lamentations":
-				return "25";
-				break;
-			case "Ezekiel":
-				return "26";
-				break;
-			case "Daniel":
-				return "27";
-				break;
-			case "Hosea":
-				return "28";
-				break;
-			case "Joel":
-				return "29";
-				break;
-			case "Amos":
-				return "30";
-				break;
-			case "Obadiah":
-				return "31";
-				break;
-			case "Jonah":
-				return "32";
-				break;
-			case "Micah":
-				return "33";
-				break;
-			case "Nahum":
-				return "34";
-				break;
-			case "Habakkuk":
-				return "35";
-				break;
-			case "Zephaniah":
-				return "36";
-				break;
-			case "Haggai":
-				return "37";
-				break;
-			case "Zechariah":
-				return "38";
-				break;
-			case "Malachi":
-				return "39";
-				break;
-			case "Matthew":
-				return "40";
-				break;
-			case "Mark":
-				return "41";
-				break;
-			case "Luke":
-				return "42";
-				break;
-			case "John":
-				return "43";
-				break;
-			case "Acts":
-				return "44";
-				break;
-			case "Romans":
-				return "45";
-				break;
-			case "1 Corinthians":
-				return "46";
-				break;
-			case "2 Corinthians":
-				return "47";
-				break;
-			case "Galatians":
-				return "48";
-				break;
-			case "Ephesians":
-				return "49";
-				break;
-			case "Philippians":
-				return "50";
-				break;
-			case "Colosians":
-				return "51";
-				break;
-			case "1 Thessalonians":
-				return "52";
-				break;
-			case "2 Thessalonians":
-				return "53";
-				break;
-			case "1 Timothy":
-				return "54";
-				break;
-			case "2 Timothy":
-				return "55";
-				break;
-			case "Titus":
-				return "56";
-				break;
-			case "Philemon":
-				return "57";
-				break;
-			case "Hebrews":
-				return "58";
-				break;
-			case "James":
-				return "59";
-				break;
-			case "1 Peter":
-				return "60";
-				break;
-			case "2 Peter":
-				return "61";
-				break;
-			case "1 John":
-				return "62";
-				break;
-			case "2 John":
-				return "63";
-				break;
-			case "3 John":
-				return "64";
-				break;
-			case "Jude":
-				return "65";
-				break;
-			case "Revelations":
-				return "66";
-				break;
-			default:
-				return "01";
-				break;
-		}
+		//TODO: replace with sql query:
+		//SELECT B from key_abbreviations_english WHERE a='';
+		return "01";
+
 	}
 	
 	public function sql() {
 		if ($this->range) {
-			return $this->book.$this->chapter.$this->verse." and ".$this->book.$this->chapter.$this->endverse;
+			return "BETWEEN (".$this->book.$this->chapter.$this->verse." and ".$this->book.$this->chapter.$this->endverse.")";
 		} else {
-			return $this->book.$this->chapter.$this->verse;
+			return "='".$this->book.$this->chapter.$this->verse."'";
 		}
 	}
     	
--- web/index.php
+++ web/index.php
@@ -1,28 +1,18 @@
 <?php
 
-//$mysqli = new mysqli('localhost', 'bible', 'bible', 'bible');
+// TEST: /index.php?b=Exodus%2011,Philippians%201:2,Psalms%2019,1%20John%201:9,Song%20Of%20Solomon%209:6&r=0
+
+$mysqli = new mysqli('localhost', 'bible', 'bible', 'bible');
 
 /*
  * This is the "official" OO way to do it,
  * BUT $connect_error was broken until PHP 5.2.9 and 5.3.0.
  */
-//if ($mysqli->connect_error) {
-//    die('Connect Error (' . $mysqli->connect_errno . ') '
-//            . $mysqli->connect_error);
-//}
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+}
 
-/*
- * Use this instead of $connect_error if you need to ensure
- * compatibility with PHP versions prior to 5.2.9 and 5.3.0.
- */
-//if (mysqli_connect_error()) {
-//    die('Connect Error (' . mysqli_connect_errno() . ') '
-//            . mysqli_connect_error());
-//}
-
-//echo 'Success... ' . $mysqli->host_info . "\n";
-
-//$mysqli->close();
 echo "Hello Handsome<br />";
 require("bible_to_sql.php");
 echo "b: ".$_GET['b']." r: ".$_GET['r']."<br />";
@@ -33,11 +23,11 @@ $references = explode(",",$_GET['b']);
 
 foreach ($references as $r) {
 			
-	$test = new bible_to_sql($r, $range);
+	$test = new bible_to_sql($r, NULL, $mysqli);
 	echo "sql: " . $test->sql() . "<br />";
 
 }
 
-//$test = new bible_to_sql($_GET['b'],$_GET['r']);
-//echo "sql: " . $test->sql();
+
+$mysqli->close();
 ?>
