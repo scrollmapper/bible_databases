39d6f95d4fa8434256b1f27d8730f3dbda744e05
Andrew Wippler <andrew.wippler@lancasterbaptist.org>
2015-01-07 22:20:36 -0800

    fixing typos

--- sql/key_abbreviations_english.sql
+++ sql/key_abbreviations_english.sql
@@ -27,9 +27,9 @@ SET time_zone = "+00:00";
 --
 DROP TABLE IF EXISTS `key_abbreviations_english`;
 CREATE TABLE `key_abbreviations_english` (
-  `id` smallint(1) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Abbreviation ID',
-  `a` varchar(251) NOT NULL,
-  `b` smallint(1) unsigned NOT NULL COMMENT 'ID of book that is abbreviated',
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Abbreviation ID',
+  `a` varchar(255) NOT NULL,
+  `b` smallint(5) unsigned NOT NULL COMMENT 'ID of book that is abbreviated',
   `p` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Whether an abbreviation is the primary one for the book',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='A table mapping book abbreviations to the book they refer to' AUTO_INCREMENT=410 ;
