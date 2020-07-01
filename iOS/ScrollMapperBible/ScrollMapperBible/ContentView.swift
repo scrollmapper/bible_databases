2531b78e3c8c09c9472855c170a018f6e7a57e37
John Kuang <john.kuang@robotsandpencils.com>
2020-07-01 09:46:31 -0600

    iOS starter

---
+++ iOS/ScrollMapperBible/ScrollMapperBible/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScrollMapperBible
+//
+//  Created by Zhengqian Kuang on 2020-07-01.
+//  Copyright © 2020 Kuang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
